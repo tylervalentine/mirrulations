566824f62eeebff2b382c243b1bc32871188e83d
lapatchakjrj <lapatchakjrj@moravian.edu>
2021-02-27 17:49:51 -0500

    added basic_work_gen.py

---
+++ src/c21server/c21work_gen/basic_work_gen.py
@@ -0,0 +1,22 @@
+import redis
+import random
+import time
+
+r = redis.Redis()
+
+
+def generateJobs(r):
+    for i in range(10):
+        id = random.randint(0, 10000)
+        value = random.randint(0, 10)
+        r.hset("jobs_wating", id, value)
+
+
+def emulateJobCreation(r):
+    for i in range(5):
+        generateJobs(r)
+        time.sleep(30)
+
+
+emulateJobCreation(r)
+
