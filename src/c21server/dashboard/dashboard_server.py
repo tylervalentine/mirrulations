a59803f34664d11ab42464e11c2dc82b3bbc0d20
Francis Severino <francisjsg2@gmail.com>
2021-03-01 10:16:13 -0500

    Added /dashboard endpoint that renders the dashboard

--- src/c21server/dashboard/dashboard_server.py
+++ src/c21server/dashboard/dashboard_server.py
@@ -1,7 +1,17 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 from job_stats import get_jobs_stats
 
 app = Flask(__name__)
+
+@app.route('/dashboard', methods=['GET'])
+def index():
+    job_information = get_jobs_stats();
+    return render_template(
+        'index.html', jobs_waiting=job_information['num_jobs_waiting'],
+        jobs_in_progress=job_information['num_jobs_in_progress'],
+        jobs_done=job_information['num_jobs_done'],
+        jobs_total=job_information['jobs_total']
+    )
 
 @app.route('/data', methods=['GET'])
 def get_dashboard_data():
