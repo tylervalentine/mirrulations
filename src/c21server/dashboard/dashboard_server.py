06d33596c07c3d1ae693e31266ceb55a423cbeae
Francis Severino <francisjsg2@gmail.com>
2021-03-01 10:14:46 -0500

    /data endpoint uses get_jobs_stats helper method

--- src/c21server/dashboard/dashboard_server.py
+++ src/c21server/dashboard/dashboard_server.py
@@ -1,13 +1,12 @@
-from redis import Redis
 from flask import Flask, jsonify
+from job_stats import get_jobs_stats
 
-r = Redis()
 app = Flask(__name__)
 
 @app.route('/data', methods=['GET'])
 def get_dashboard_data():
-    return jsonify({'num_jobs_waiting' : int(r.hlen('jobs_waiting'))}), 200
-
+    job_information = get_jobs_stats();
+    return jsonify(job_information), 200
 
 if __name__ == '__main__':
     app.run(port=5000)
