43cc8eea5ea61e6b4dfedb683d0e950439d6b520
Kylie Norwood <norwoodk@moravian.edu>
2021-03-08 17:28:25 -0500

    Begin testing the client

--- tests/c21client/test_client.py
+++ tests/c21client/test_client.py
@@ -1,5 +1,10 @@
-from c21client.client import dummy_client
+from c21client.client import try_to_get_work
+from c21server.work_server.work_server import WorkServer, create_server
+from fakeredis import FakeStrictRedis
+from flask import Flask, json, jsonify, request
 
+@fixture
+def mock_db():
+    mock_db = FakeStrictRedis()
+    server = create_server(mock_db)
 
-def test_dummy():
-    assert dummy_client() == 42
