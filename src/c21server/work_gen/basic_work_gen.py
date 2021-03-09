ff5ed4b06e4ef40dabb63bac3ead46dd29839637
Riley Kirkpatrick <kirkpatrickr@moravian.edu>
2021-03-08 20:51:24 -0500

    Update to make easier to test

--- src/c21server/work_gen/basic_work_gen.py
+++ src/c21server/work_gen/basic_work_gen.py
@@ -3,20 +3,22 @@ import time
 import redis
 
 
-def generate_jobs(database):
+def generate_jobs(database, start_key=None):
+    if start_key is None:
+        start_key = random.randint(0, 10)
     for _ in range(10):
-        key = random.randint(0, 10000)
         value = random.randint(0, 10)
-        print(f"I am generating  work with value {value}!")
-        database.hset("jobs_waiting", key, value)
+        print(f"I am generating work {start_key} with value {value}!")
+        database.hset("jobs_waiting", start_key, value)
+        start_key += 1
 
 
-def emulate_job_creation():
-    database = redis.Redis()
-    for _ in range(5):
-        generate_jobs(database)
+def emulate_job_creation(database):
+    for current_key in range(1, 50, 10):
+        generate_jobs(database, current_key)
         time.sleep(30)
 
 
 if __name__ == "__main__":
-    emulate_job_creation()
+    redis = redis.Redis()
+    emulate_job_creation(redis)
