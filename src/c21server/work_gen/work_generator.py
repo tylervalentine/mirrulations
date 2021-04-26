89b2fc632a2c0ba22d35f1873a281f2fbc1d734e
Jonah Beers <beersj02@moravian.edu>
2021-04-26 14:35:05 -0400

    Save lastModifiedDate for work generator to use

--- src/c21server/work_gen/work_generator.py
+++ src/c21server/work_gen/work_generator.py
@@ -1,5 +1,5 @@
 import os
-import time
+from time import sleep
 from datetime import datetime, timedelta
 import requests
 import dotenv
@@ -10,78 +10,81 @@ dotenv.load_dotenv()
 API_TOKEN = os.getenv('API_TOKEN')
 
 
+def create_jobs(database):
+    get_jobs('dockets', database)
+    get_jobs('documents', database)
+    get_jobs('comments', database)
+
+
+def get_jobs(endpoint, database):
+    params = {
+        'api_key': API_TOKEN,
+        'sort': 'lastModifiedDate',
+        'page[size]': 250
+    }
+
+    if database.exists('last_modified_date'):
+        date = database.get('last_modified_date')
+        params['filter[lastModifiedDate][ge]'] = date
+
+    if not os.path.exists(f'{f"regulations/{endpoint}"}'):
+        os.makedirs(f'regulations/{endpoint}')
+
+    url = f'https://api.regulations.gov/v4/{endpoint}'
+    items = make_request(url, params=params)
+    total_elements = int(items.json()['meta']['totalElements'])
+
+    while total_elements > 0:
+        try:
+            for page in range(1, 21):
+                params['page[number]'] = str(page)
+                items = make_request(url, params).json()
+                total_elements -= write_endpoints(endpoint, database, items)
+            update_filter(url, params)
+        except IndexError:
+            print('Ran out of items to download')
+
+
 def make_request(url, params):
     response = requests.get(url, params=params)
     if response.status_code == 429:
-        time.sleep(3600)
+        sleep(3600)
         response = requests.get(url, params=params)
     response.raise_for_status()
     return response
 
 
-def transform_date(response):
-    """Expects date in this format yyyy-mm-dd hh:mm:ss"""
-    date = response.json()['data'][-1]['attributes']['lastModifiedDate']
-    date = date.replace("T", " ").replace("Z", "")
-
-    return str(datetime.fromisoformat(date) + timedelta(hours=-4))
+def write_endpoints(endpoint, database, items):
+    counter = 0
+    for item in items['data']:
+        database.incr('next_job_id')
+        job_id = database.get('next_job_id')
+        job = f'{endpoint}/{item["id"]}'
+        database.hset('jobs_waiting', job_id, job)
+        date = item['attributes']['lastModifiedDate']
+        database.set('last_modified_date', transform_date(date))
+        counter += 1
+    return counter
 
 
 def update_filter(url, params):
     response = make_request(url, params)
     response.raise_for_status()
-    params["filter[lastModifiedDate][ge]"] = transform_date(response)
+    date = response.json()['data'][-1]['attributes']['lastModifiedDate']
+    params['filter[lastModifiedDate][ge]'] = transform_date(date)
 
 
-def write_endpoints(endpoint, database):
-    params = {
-        "api_key": API_TOKEN,
-        "sort": 'lastModifiedDate',
-        "page[size]": 250
-    }
-
-    if not os.path.exists(f"{f'regulations/{endpoint}'}"):
-        os.makedirs(f'regulations/{endpoint}')
-
-    url = f'https://api.regulations.gov/v4/{endpoint}'
-
-    results = make_request(url, params=params)
-    total_elements = int(results.json()['meta']['totalElements'])
-
-    while total_elements > 0:
-        try:
-            for page in range(1, 21):
-                params["page[number]"] = str(page)
-                items = make_request(url, params).json()
-                total_elements -= getting_jobs(endpoint, database, items)
-            update_filter(url, params)
-        except IndexError:
-            print("ran out of items to download")
-
-
-def getting_jobs(endpoint, database, items):
-    counter = 0
-    for item in items['data']:
-        temp_job = database.get('temp_job')
-        job = f"{endpoint}/{item['id']}"
-        job_id = int(temp_job)if temp_job is not None else 0
-        database.hset("jobs_waiting", job_id, job)
-        database.incr('temp_job')
-        counter += 1
-    return counter
-
-
-def create_jobs(database):
-    write_endpoints('dockets', database)
-    write_endpoints('documents', database)
-    write_endpoints('comments', database)
+def transform_date(date):
+    date = date.replace('T', ' ').replace('Z', '')
+    return str(datetime.fromisoformat(date) + timedelta(hours=-4))
 
 
 if __name__ == '__main__':
     redis = redis.Redis()
     try:
         redis.ping()
-        print('Successfully connected to redis')
+        print('Successfully connected to redis!\nGetting new jobs...')
         create_jobs(redis)
+        print('Done!')
     except redis.exceptions.ConnectionError as r_con_error:
         print('Redis connection error:', r_con_error)
