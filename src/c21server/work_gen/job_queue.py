48947bba05cc6c1fcac22e1180cd9dfd208ae398
Ben Coleman <colemanb@moravian.edu>
2021-06-09 13:20:12 -0400

    Store jobs as dict with job_id

--- src/c21server/work_gen/job_queue.py
+++ src/c21server/work_gen/job_queue.py
@@ -1,11 +1,22 @@
+import json
 
 class JobQueue:
 
     def __init__(self, database):
         self.database = database
 
-    def add_job(self, job):
-        self.database.lpush('jobs_waiting_queue', job)
+    def add_job(self, url):
+        job_id = self.get_job_id()
+        job = {'job_id': job_id,
+               'url': url}
+        self.database.lpush('jobs_waiting_queue', json.dumps(job))
 
     def get_num_jobs(self):
         return self.database.llen('jobs_waiting_queue')
+
+    def get_job(self):
+        return json.loads(self.database.lpop('jobs_waiting_queue'))
+
+    def get_job_id(self):
+        job_id = self.database.incr('last_job_id')
+        return job_id
--- tests/c21server/test_job_queue.py
+++ tests/c21server/test_job_queue.py
@@ -3,11 +3,26 @@ from fakeredis import FakeRedis
 from c21server.work_gen.job_queue import JobQueue
 
 
-def test_job_added_with_next_id():
+def test_first_job_added_with_id_0():
     database = FakeRedis()
-
     queue = JobQueue(database)
 
     queue.add_job('http://a.b.c')
 
     assert queue.get_num_jobs() == 1
+    job = queue.get_job()
+    assert job['job_id'] == 1
+    assert job['url'] == 'http://a.b.c'
+
+
+def test_job_added_with_next_id():
+    database = FakeRedis()
+    database.set('last_job_id', 42)
+    queue = JobQueue(database)
+
+    queue.add_job('http://a.b.c')
+
+    assert queue.get_num_jobs() == 1
+    job = queue.get_job()
+    assert job['job_id'] == 43
+    assert job['url'] == 'http://a.b.c'
