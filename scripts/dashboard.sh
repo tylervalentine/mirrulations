a703d35ae54a0867d03ba13fb1b2ed69c0243b21
Ben Coleman <colemanb@moravian.edu>
2021-06-21 12:31:35 -0400

    Remove vagrant-based production environment

--- scripts/dashboard.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd /home/vagrant/capstone2021
-source .venv/bin/activate
-sudo python3 src/c21server/dashboard/dashboard_server.py
--- scripts/workgen.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd /home/vagrant/capstone2021
-.venv/bin/python src/c21server/work_gen/basic_work_gen.py
