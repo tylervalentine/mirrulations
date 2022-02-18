51336fe2e4bdf793a97e5434dee91771453975d1
Matthew Kosack <kosackm@moravian.edu>
2022-02-18 16:23:45 -0500

    fix tab issues

--- mirrulations-core/src/mirrcore/redis_check.py
+++ mirrulations-core/src/mirrcore/redis_check.py
@@ -1,7 +1,7 @@
 import redis
 
 def is_redis_available(database):
-        try:
-            return database.ping()
-        except (ConnectionRefusedError, redis.BusyLoadingError):
-            return False
+    try:
+        return database.ping()
+    except (ConnectionRefusedError, redis.BusyLoadingError):
+        return False
