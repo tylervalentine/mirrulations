19114c6da5c08323f388a607cbc000d87add13f1
Michael Marchese <marchesem@moravian.edu>
2022-02-25 16:04:20 -0500

    added useful import statements to the wsgi.py file

--- mirrulations-dashboard/src/mirrdash/wsgi.py
+++ mirrulations-dashboard/src/mirrdash/wsgi.py
@@ -1,6 +1,8 @@
 import docker
 from redis import Redis
 from mirrdash.dashboard_server import create_server
+from mirrdash.sum_mongo_counts import connect_mongo_db
+import os
 
 server = create_server(Redis('redis'), docker.from_env(),connect_mongo_db(os.getenv('MONGO_HOSTNAME'),27017))
 app = server.app
