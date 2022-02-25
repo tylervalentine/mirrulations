b6ba3fde963526b750ea5eb60dc80dcf2ab7d88a
AbdullahAlramyan <stana47@moravian.edu>
2022-02-25 15:47:12 -0500

    adding a pymongo in the setup.cfg and changing the wsgi.py so that we have the call of mongo host

--- mirrulations-dashboard/src/mirrdash/wsgi.py
+++ mirrulations-dashboard/src/mirrdash/wsgi.py
@@ -2,5 +2,5 @@ import docker
 from redis import Redis
 from mirrdash.dashboard_server import create_server
 
-server = create_server(Redis('redis'), docker.from_env())
+server = create_server(Redis('redis'), docker.from_env(),connect_mongo_db(os.getenv('MONGO_HOSTNAME'),27017))
 app = server.app
