a1991545f6da2ad24992ae8121416ec0dec46dcb
maxwellschuman <IaPIjDKh>
2022-03-18 14:24:12 -0400

    fixing attachment_generator duplication issue

--- attachments_generator.py
+++ attachments_generator.py
@@ -1,3 +1,13 @@
+import os
+import time
+import dotenv
+import redis
+from mirrgen.search_iterator import SearchIterator
+from mirrgen.results_processor import ResultsProcessor
+from mirrcore.regulations_api import RegulationsAPI
+from mirrcore.job_queue import JobQueue
+from mirrcore.data_storage import DataStorage
+from mirrcore.redis_check import is_redis_available
 
 
 '''
