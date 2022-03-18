084ebf73b2aab7461e6afa0ecad912718661e6f4
maxwellschuman <IaPIjDKh>
2022-03-18 14:51:31 -0400

    created attachments_generator.md

--- attachments_generator.py
+++ attachments_generator.py
@@ -13,7 +13,7 @@ from mirrcore.redis_check import is_redis_available
 '''
 
 - this sprint we need to generate these jobs (attachments)
-- when jobs generate they also need to generate the attachments (most ikely will stay on generator side)
+- when jobs generate they also need to generate the attachments (most likely will stay on generator side)
 - there will be the one time 15m job script that we will run and we will deploy the process of keeping up (next sprint)
 - For now envision a program that basically for count in range (some have attachments)
 - its not about downlaod its about the download_job_type : attachments
@@ -26,11 +26,11 @@ from mirrcore.redis_check import is_redis_available
 - put together in a dict do a json dump s turn into string put that in job loading queue
 - because we understand jobs waiting queue and the last_job_id you know what it is supposed to look like you can use that code to load it in a redis database
 - interact with redis in that form
+
 COLEMAN IDEA
 - add another dockercontainer to the system that will encapsulate that little script 
 - done in PR add DC
-- make container then gets launched normal with devup and it runs and does its thing 
-- that can work OR
+- IDEA: make container then gets launched normal with devup
 - could be a standalone script and i beleive in the docker compsoe file we expose the port for docker 
 - 6379 if you do a devup if you have it installed on your machine the redis port will be mapped from the redis port to the local host
 - attachments generator
