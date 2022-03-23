261739e7ee351fb1a5384adbde30923ae43b6cba
eric <gorskie@moravian.edu>
2022-03-23 14:52:12 -0400

    hardcoded vallues for the dashboard calls to redis, added shell of func for incr/decr redis db

---
+++ mirrulations-core/src/mirrcore/redis_queue_utils.py
@@ -0,0 +1,10 @@
+"""
+This module contains the functions that will increment and decrement the counts of job
+    types in the queues
+
+Dependencies:
+    redis
+"""
+
+def change_queue_counter(redis_db, job_type, increment=True):
+    pass
