ca899f5b5bad932b93d5d3672be7721da201be3d
robertrabinovich <rabinovichr@moravian.edu>
2022-03-25 18:07:35 -0400

    Fixed linter errors for test_jobs_queued_utils

--- mirrulations-dashboard/tests/test_jobs_queued_utils.py
+++ mirrulations-dashboard/tests/test_jobs_queued_utils.py
@@ -1,11 +1,8 @@
-from operator import ge
-from unittest import mock
-from webbrowser import get
-from fakeredis import FakeRedis
 from mirrdash.jobs_queued_utils import get_jobs_queued_stats, \
     get_jobs_queued_attachments, get_jobs_queued_comments, \
-        get_jobs_queued_dockets, get_jobs_queued_documents
+    get_jobs_queued_dockets, get_jobs_queued_documents
 from mirrmock.mock_redis import MockRedisWithStorage
+
 
 def test_jobs_queued_stats():
     mock_database = MockRedisWithStorage()
@@ -13,23 +10,28 @@ def test_jobs_queued_stats():
     mock_database.set('num_jobs_comments_queued', 2)
     mock_database.set('num_jobs_dockets_queued', 3)
     mock_database.set('num_jobs_documents_queued', 4)
-    assert get_jobs_queued_stats(mock_database) == {'num_jobs_attachments_queued': 1, 'num_jobs_comments_queued': 2, \
-        'num_jobs_dockets_queued': 3, 'num_jobs_documents_queued': 4}
+    assert get_jobs_queued_stats(mock_database) == \
+        {'num_jobs_attachments_queued': 1, 'num_jobs_comments_queued': 2,
+            'num_jobs_dockets_queued': 3, 'num_jobs_documents_queued': 4}
+
 
 def test_attachments_queued():
     mock_database = MockRedisWithStorage()
     mock_database.set('num_jobs_attachments_queued', 5)
     assert get_jobs_queued_attachments(mock_database) == 5
 
+
 def test_comments_queued():
     mock_database = MockRedisWithStorage()
     mock_database.set('num_jobs_comments_queued', 5)
     assert get_jobs_queued_comments(mock_database) == 5
 
+
 def test_dockets_queued():
     mock_database = MockRedisWithStorage()
     mock_database.set('num_jobs_dockets_queued', 5)
     assert get_jobs_queued_dockets(mock_database) == 5
+
 
 def test_documents_queued():
     mock_database = MockRedisWithStorage()
