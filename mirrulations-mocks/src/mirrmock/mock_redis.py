a09853e432b4afd6c3ceffd977e30365cf647292
Isaac Wood <woodi@moravian.edu>
2022-03-28 12:09:05 -0400

    added incr and decr to mock_redis, made tests for redis_queue_utils

--- mirrulations-mocks/src/mirrmock/mock_redis.py
+++ mirrulations-mocks/src/mirrmock/mock_redis.py
@@ -26,8 +26,16 @@ class MockRedisWithStorage():
 
         
     def set(self, key, value):
+        if key is None:
+            self.data[key] = int(0)
         self.data[f'{key}'] = value
 
 
     def get(self, key):
         return self.data[f'{key}']
+
+    def incr(self, key):
+        self.data[key] += 1
+
+    def decr(self, key):
+        self.data[key] -= 1
