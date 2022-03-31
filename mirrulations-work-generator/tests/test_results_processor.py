5f16b3f53a362ef9fde71475613fb48944c2193f
Kimberly Miller <millerk10@moravian.edu>
2022-03-31 19:57:25 -0400

    Removed unused import statements

--- mirrulations-work-generator/tests/test_results_processor.py
+++ mirrulations-work-generator/tests/test_results_processor.py
@@ -1,12 +1,10 @@
 import json
 import os
-from unittest import result
 from fakeredis import FakeRedis
 from mirrgen.results_processor import ResultsProcessor
 from mirrcore.job_queue import JobQueue
 from mirrmock.mock_data_storage import MockDataStorage
 from mirrmock.mock_dataset import MockDataSet
-from pprint import pprint
 
 
 def test_process_results():
@@ -22,10 +20,10 @@ def test_process_results():
 
     assert database.llen('jobs_waiting_queue') == 10
 
+
 def test_adds_two_jobs_if_is_comment():
     database = FakeRedis()
     results = MockDataSet(1, job_type='comments').get_results()
     processor = ResultsProcessor(JobQueue(database), MockDataStorage())
-    
     processor.process_results(json.loads(results[0]['text']))
     assert database.llen('jobs_waiting_queue') == 2
