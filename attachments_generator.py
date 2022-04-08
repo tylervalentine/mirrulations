99c5086b56860dae53cc4b4dcbcd64052049d811
Kimberly Wolf <wolfk@moravian.edu>
2022-04-08 15:08:55 -0400

    Fixed variable names

--- attachments_generator.py
+++ attachments_generator.py
@@ -16,28 +16,29 @@ class AttachmentsGenerator:
 
     * Create a new job with those links
     '''
-        def read_attachments_csv():
-            data_frame = pd.read_csv('/home/cs334/capstone2022/attachments_list.csv', usecols=['commentId',
-                    'attachments'], dtype={'commentId':'str','attachments':'str'})
-            return data_frame
+    def read_attachments_csv():
+        data_frame = pd.read_csv('/home/cs334/capstone2022/attachments_list.csv', usecols=['data.id',
+                'data.relationships.attachments.links.related'], dtype={'data.id':'str',
+                'data.relationships.attachments.links.related':'str'})
+        return data_frame
 
-        def __init__(self, job_queue, database):
-            self.job_queue = job_queue
-            self.database = database
+    def __init__(self, job_queue, database):
+        self.job_queue = job_queue
+        self.database = database
 
-        def get_job_id(self):
-            job_id = self.database.incr('last_job_id')
-            return job_id
+    def get_job_id(self):
+        job_id = self.database.incr('last_job_id')
+        return job_id
 
-        def add_job(self, job_type, url):
-            job_id = self.get_job_id()
-            if job_type == 'attachments':
-                job = {'job_id': job_id,
-                        'url': url,
-                        'job_type': job_type
-                    }
+    def add_job(self, job_type, url):
+        job_id = self.get_job_id()
+        if job_type == 'attachments':
+            job = {'job_id': job_id,
+                    'url': url,
+                    'job_type': job_type
+                }
 
-    if __name__ == '__main__':
+if __name__ == '__main__':
     database = redis.Redis()
     while not is_redis_available(database):
         print("Redis database is busy loading")
@@ -51,17 +52,17 @@ class AttachmentsGenerator:
     attachments_list = []
 
     # This is how the attachments generator was working during the last sprint
-    data_frame = read_attachments_csv()
-    for link in data_frame['attachments']:
+    data_frame = generator.read_attachments_csv()
+    for link in data_frame['data.relationships.attachments.links.related']:
         attachments_list.append(link)
 
-    for i in attachments_list:
-        job = generator.add_job('attachments', i)
-            attachments_index += 1
+    for link in attachments_list:
+        job = generator.add_job('data.relationships.attachments.links.related', link)
+        attachments_index += 1
 
     '''
     # Would this work???
-    for link in data_frame['attachments']:
+    for link in data_frame['data.relationships.attachments.links.related']:
         job = generator.add_job('attachments', link)
     '''
 
