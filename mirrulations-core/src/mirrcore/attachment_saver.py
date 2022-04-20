9c690c6a66922af5a0658c901019e86f1b51d502
Richie Glennon <glennonr@moravian.edu>
2022-04-19 23:27:10 -0400

    split attachment code in data_storage and fixed path in attachment saver

--- mirrulations-core/src/mirrcore/attachment_saver.py
+++ mirrulations-core/src/mirrcore/attachment_saver.py
@@ -5,7 +5,7 @@ class AttachmentSaver():
     def __init__(self):
         pass
 
-    def save(self, data, path=os.path.join(os.getcwd(), 'mirrulations-core/tests/test_put_files')):
+    def save(self, data, path=os.path.join(os.getcwd())):
         for key in data['results'].keys():
             with open(os.path.join(path, key), 'wb') as f:
                 f.write(base64.b64decode(data['results'][key].encode('ascii')))
