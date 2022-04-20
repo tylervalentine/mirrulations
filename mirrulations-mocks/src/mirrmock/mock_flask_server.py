3b16ef0a947561e4ec0520c8e927affc56b694f4
Shane Houghton <houghtons@moravian.edu>
2022-04-20 15:15:07 -0400

    wrote mocker for testing workserver dealing with attachments

--- mirrulations-mocks/src/mirrmock/mock_flask_server.py
+++ mirrulations-mocks/src/mirrmock/mock_flask_server.py
@@ -3,14 +3,24 @@ from fakeredis import FakeRedis, FakeServer
 from mirrmock.mock_data_storage import MockDataStorage
 
 
+class mockAttachmentSaver():
+    def __init__(self):
+        self.num_attachments = 0
+        self.data = None
+    
+    def save(self, data):
+        self.num_attachments += 1
+        self.data = data
 
 
 def mock_work_server(create_server):
     redis_server = FakeServer()
     mock_db = FakeRedis(server=redis_server)
+    mock_saver = mockAttachmentSaver()
     server = create_server(mock_db)
     server.redis_server = redis_server
     server.app.config['TESTING'] = True
     server.client = server.app.test_client()
     server.data = MockDataStorage()
+    server.attachment_saver = mock_saver
     return server
