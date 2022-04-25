5308700c78db29b6e31c01fcd3164a69d2eb4267
Kimberly Miller <millerk10@moravian.edu>
2022-04-25 13:19:41 -0400

    Updated in-line comments

--- mirrulations-work-generator/src/mirrgen/results_processor.py
+++ mirrulations-work-generator/src/mirrgen/results_processor.py
@@ -8,16 +8,18 @@ class ResultsProcessor:
     def process_results(self, results_dict):
         for item in results_dict['data']:
             if not self.data_storage.exists(item):
+                # sets url and job_type
                 url = item['links']['self']
                 job_type = item['type']
-
+                # adds current job to jobs_waiting_queue
                 self.job_queue.add_job(url, job_type)
+                # checks to see if it has an attachment
                 has_attachment = 'relationships' in item
                 if job_type == 'comments' and has_attachment is True:
                     relatsh = 'relationships'
                     attm = 'attachments'
-                    # grab api call for attachments of that comments
+                    # updates the url and job_type
                     url = item[relatsh][attm]['links']['related']
                     job_type = 'attachments'
-                    # add new attachment job
+                    # adds new attachment job to jobs_waiting_queue
                     self.job_queue.add_job(url, job_type)
