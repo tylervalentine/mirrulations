bcf2485c04cc3528bfbcc8a618b83aae858f5f0e
Justin Szaro <justinszaro@gmail.com>
2023-02-19 14:13:50 -0500

    Add docket path function and test

--- mirrulations-core/src/mirrcore/path_generator.py
+++ mirrulations-core/src/mirrcore/path_generator.py
@@ -1,2 +1,17 @@
-def fake_test():
-    assert True
+import os
+
+
+class PathGenerator:
+
+    def __init__(self):
+        self.path = "data"
+
+    def get_path(self, job):
+        if job['job_type'] == 'dockets':
+            return get_docket_path(job)
+
+    def get_docket_path(self, job): 
+        job = job['data']
+        job_id = job['id']
+        agency, year, docket_id = job_id.split('-')
+        return f'data/{agency}/{year}/{job_id}/text-{job_id}/docket/'
--- mirrulations-core/tests/test_path_generator.py
+++ mirrulations-core/tests/test_path_generator.py
@@ -1,4 +1,11 @@
-class PathGenerator:
+from mirrcore.path_generator import PathGenerator
 
-    def __init__(self, job):
-        self.job = job
+def test_get_docket_path():
+    job = {
+        "data": {
+            "id": "VETS-2005-0001",
+            "type": "dockets"
+        }  
+    } 
+    actual_path = "data/VETS/2005/VETS-2005-0001/text-VETS-2005-0001/docket/"
+    assert actual_path == PathGenerator().get_docket_path(job)
