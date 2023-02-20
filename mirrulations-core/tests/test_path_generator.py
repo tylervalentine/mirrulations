e8773309290c26bb51380b99ef841f9ec95f39a6
Edwin <cojitamboe@moravian.edu>
2023-02-19 22:49:13 -0500

    added tests for documents and comments

--- mirrulations-core/tests/test_path_generator.py
+++ mirrulations-core/tests/test_path_generator.py
@@ -21,3 +21,23 @@ def test_get_docket_path_from_FRDOC_docket():
     expected_path = "data/VETS/FRDOCS/VETS_FRDOC_0001/text-VETS_FRDOC_0001/docket/"
     assert expected_path == PathGenerator().get_docket_path(job)
 
+
+def test_get_document_path():
+    job = {
+        "data": {
+            "id": "USTR-2015-0010-0001",
+            "type": "documents"
+        }  
+    } 
+    actual_path = "data/USTR/2015/USTR-2015-0010/text-USTR-2015-0010/documents"
+    assert actual_path == PathGenerator().get_document_text_path(job)
+
+def test_get_comment_path():
+    job = {
+        "data": {
+            "id": "USTR-2015-0010-0002",
+            "type": "comments"
+        }  
+    } 
+    actual_path = "data/USTR/2015/USTR-2015-0010/text-USTR-2015-0010/comments"
+    assert actual_path == PathGenerator().get_comment_text_path(job)
