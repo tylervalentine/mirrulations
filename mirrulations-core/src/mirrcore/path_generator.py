7656fd9fbcba78ff4545af887e67028e3d0f2bde
Jack Wagner <jacksonjw519@gmail.com>
2023-02-20 00:04:37 -0500

    FRDOC document handling

--- mirrulations-core/src/mirrcore/path_generator.py
+++ mirrulations-core/src/mirrcore/path_generator.py
@@ -17,6 +17,21 @@ class PathGenerator:
         agency, year, docket_id = job_id.split('-')
         return f'data/{agency}/{year}/{job_id}/text-{job_id}/docket/'
 
+
+    def get_document_path(self, job):
+        job = job['data']
+        job_id = job['id']
+        if "FRDOC" in job_id:
+            agency, FRDOC, rest_of_id = job_id.split("_")
+            docket_id, document_id = rest_of_id.split("-")
+            docket_id = '_'.join([agency, FRDOC, docket_id])
+            return f'data/{agency}/FRDOCS/{docket_id}/text-{docket_id}/documents/'
+
+        agency, year, docket_id = job_id.split('-')
+        return f'data/{agency}/{year}/{job_id}/text-{job_id}/docket/'
+
     def get_path(self, job):
         if job['job_type'] == 'dockets':
             return get_docket_path(job)
+        if job['job_type'] == "documents":
+            return get_document_path(job)
--- mirrulations-core/tests/test_path_generator.py
+++ mirrulations-core/tests/test_path_generator.py
@@ -21,3 +21,13 @@ def test_get_docket_path_from_FRDOC_docket():
     expected_path = "data/VETS/FRDOCS/VETS_FRDOC_0001/text-VETS_FRDOC_0001/docket/"
     assert expected_path == PathGenerator().get_docket_path(job)
 
+def test_get_docket_path_from_FRDOC_document():
+    job = {
+        "data":{
+            "id": "VETS_FRDOC_0001-0001", 
+            "type": "documents"
+        }
+    }
+    expected_path = "data/VETS/FRDOCS/VETS_FRDOC_0001/text-VETS_FRDOC_0001/documents/"
+    assert expected_path == PathGenerator().get_document_path(job)
+
