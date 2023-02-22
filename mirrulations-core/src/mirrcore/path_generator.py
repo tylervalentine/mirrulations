b6b61184bafff2ae52acfbfa442cfed56f20bc25
Justin Szaro <justinszaro@gmail.com>
2023-02-21 20:46:46 -0500

    Made all tests pass

--- mirrulations-core/src/mirrcore/path_generator.py
+++ mirrulations-core/src/mirrcore/path_generator.py
@@ -3,37 +3,18 @@ import re
 
 class PathGenerator:
 
-    def __init__(self):
-        self.path = "data"
-
-    def get_path(self, json):
-        try: 
-            type = json["data"]["type"]
-        except KeyError:
-            print("JSON did not have 'type' key")
-            raise KeyError
-
-        if type == "dockets":
-            return self.get_docket_path(json)
-
-        elif type == "documents":
-            return self.get_document_text_path(json)
-
-        elif type == "comments": 
-            return self.get_comment_text_path(json)
-
-    def get_docket_path(self, json): 
+    def get_docket_json_path(self, json): 
         try: 
             id, agencyId = json["data"]["id"], json["data"]["attributes"]["agencyId"]
         except KeyError:
             print("Could not find necessary keys in json")
             raise KeyError
         if "FRDOC" in id:
-            return f'data/{agencyId}/{id}/text-{id}/dockets/{id}.json'
-        return f'data/{agencyId}/{id}/text-{id}/dockets/{id}.json'
+            return f'data/{agencyId}/{id}/text-{id}/docket/'
+        return f'data/{agencyId}/{id}/text-{id}/docket/'
 
 
-    def get_document_text_path(self, json):
+    def get_document_json_path(self, json):
         try: 
             id, agencyId = json["data"]["id"], json["data"]["attributes"]["agencyId"]
         except KeyError:
@@ -45,26 +26,24 @@ class PathGenerator:
             agency, FRDOC, rest_of_id = id.split("_")
             docket_id= rest_of_id.split("-")[0]
             docket_id = '_'.join([agency, FRDOC, docket_id])
-            return f'data/{agencyId}/{docket_id}/text-{docket_id}/documents/{id}.json'
+            return f'data/{agencyId}/{docket_id}/text-{docket_id}/documents/'
         
         
         if re.match("([A-Z]{2,}-[1-2][0-9]{3,}-[0-9]{4,})-[0-9]{4,}", id):
             docket_id = str(re.match("([A-Z]{2,}-[1-2][0-9]{3,}-[0-9]{4,})-[0-9]{4,}", id).groups()[0])
             # agency, year, docket_num = id.split('-')[:3]
             # docket_id = "-".join([agency, year, docket_num])
-            return f'data/{agencyId}/{docket_id}/text-{docket_id}/documents/{id}.json'
+            return f'data/{agencyId}/{docket_id}/text-{docket_id}/documents/'
         
         else: 
             # data/EPA/EPA-HQ-OPP-2011-0939/text-EPA-HQ-OPP-2011-0939/documents/EPA-HQ-OPP-2011-0939-0001.json
             # Cases where id is not of standard form of AGENCYID-<YEAR>-<DocketID>-<DocumentIDorCommentID>
             split_id = id.split('-') # list of id_segments
             docket_id = '-'.join(split_id[:-1])
-            return f'data/{agencyId}/{docket_id}/text-{docket_id}/documents/{id}.json'
-                
-            
+            return f'data/{agencyId}/{docket_id}/text-{docket_id}/documents/'
 
 
-    def get_comment_text_path(self, json):
+    def get_comment_json_path(self, json):
         try: 
             id, agencyId = json["data"]["id"], json["data"]["attributes"]["agencyId"]
         except KeyError:
@@ -76,19 +55,19 @@ class PathGenerator:
             agency, FRDOC, rest_of_id = id.split("_")
             docket_id= rest_of_id.split("-")[0]
             docket_id = '_'.join([agency, FRDOC, docket_id])
-            return f'data/{agencyId}/{docket_id}/text-{docket_id}/comments/{id}.json'
+            return f'data/{agencyId}/{docket_id}/text-{docket_id}/comments/'
         
         
         if re.match("([A-Z]{2,}-[1-2][0-9]{3,}-[0-9]{4,})-[0-9]{4,}", id):
             docket_id = str(re.match("([A-Z]{2,}-[1-2][0-9]{3,}-[0-9]{4,})-[0-9]{4,}", id).groups()[0])
             # agency, year, docket_num = id.split('-')[:3]
             # docket_id = "-".join([agency, year, docket_num])
-            return f'data/{agencyId}/{docket_id}/text-{docket_id}/comments/{id}.json'
+            return f'data/{agencyId}/{docket_id}/text-{docket_id}/comments/'
         
         else: 
             # data/EPA/EPA-HQ-OPP-2011-0939/text-EPA-HQ-OPP-2011-0939/documents/EPA-HQ-OPP-2011-0939-0002.json
             # Cases where id is not of standard form of AGENCYID-<YEAR>-<DocketID>-<DocumentIDorCommentID>
             split_id = id.split('-') # list of id_segments
             docket_id = '-'.join(split_id[:-1])
-            return f'data/{agencyId}/{docket_id}/text-{docket_id}/comments/{id}.json'
+            return f'data/{agencyId}/{docket_id}/text-{docket_id}/comments/'
 
--- mirrulations-core/tests/test_path_generator.py
+++ mirrulations-core/tests/test_path_generator.py
@@ -21,86 +21,65 @@ def generate_json(id=str, type=str, agencyId=str) -> dict:
 def test_get_comment_path_full_json(generator):
     json_file = '{"data": {"attributes": {"address1": null, "address2": null, "agencyId": "USTR", "category": null, "city": "Washington", "comment": "See attached. ", "commentOn": "0900006481bce624", "commentOnDocumentId": "USTR-2015-0010-0001", "country": "United States", "displayProperties": null, "docAbstract": null, "docketId": "USTR-2015-0010", "documentType": "Public Submission", "duplicateComments": 0, "email": null, "fax": null, "field1": null, "field2": null, "fileFormats": null, "firstName": "Stephanie", "govAgency": null, "govAgencyType": null, "lastName": "Henry", "legacyId": null, "modifyDate": "2015-09-30T20:57:34Z", "objectId": "0900006481c7f7ad", "openForComment": false, "organization": "US-China Business Council", "originalDocumentId": null, "pageCount": 0, "phone": null, "postedDate": "2015-09-30T04:00:00Z", "postmarkDate": null, "reasonWithdrawn": null, "receiveDate": "2015-09-18T04:00:00Z", "restrictReason": null, "restrictReasonType": null, "stateProvinceRegion": "DC", "submitterRep": null, "submitterRepAddress": null, "submitterRepCityState": null, "subtype": null, "title": "US China Business Council", "trackingNbr": "1jz-8l73-5qbv", "withdrawn": false, "zip": null}, "id": "USTR-2015-0010-0002", "links": {"self": "https://api.regulations.gov/v4/comments/USTR-2015-0010-0002"}, "relationships": {"attachments": {"links": {"related": "https://api.regulations.gov/v4/comments/USTR-2015-0010-0002/attachments", "self": "https://api.regulations.gov/v4/comments/USTR-2015-0010-0002/relationships/attachments"}}}, "type": "comments"}}'
     json_file = json.loads(json_file)
-    expected_path = "data/USTR/USTR-2015-0010/text-USTR-2015-0010/comments"
-    assert expected_path == generator.get_path(json_file)
+    expected_path = "data/USTR/USTR-2015-0010/text-USTR-2015-0010/comments/"
+    assert expected_path == generator.get_comment_json_path(json_file)
 
 def test_get_document_path_full_json(generator):
     json_file = '{"data": {"attributes": {"additionalRins": null, "address1": null, "address2": null, "agencyId": "USTR", "allowLateComments": false, "authorDate": null, "authors": null, "category": null, "cfrPart": null, "city": null, "comment": null, "commentEndDate": null, "commentStartDate": null, "country": null, "displayProperties": null, "docAbstract": null, "docketId": "USTR-2015-0010", "documentType": "Supporting & Related Material", "effectiveDate": null, "email": null, "exhibitLocation": null, "exhibitType": null, "fax": null, "field1": null, "field2": null, "fileFormats": null, "firstName": null, "frDocNum": null, "frVolNum": null, "govAgency": null, "govAgencyType": null, "implementationDate": null, "lastName": null, "legacyId": null, "media": null, "modifyDate": "2015-10-09T15:59:17Z", "objectId": "0900006481cb1d8f", "ombApproval": null, "openForComment": false, "organization": null, "originalDocumentId": null, "pageCount": 0, "paperLength": 0, "paperWidth": 0, "phone": null, "postedDate": "2015-10-05T00:00:00Z", "postmarkDate": null, "reasonWithdrawn": "Upddate to agenda", "receiveDate": null, "regWriterInstruction": null, "restrictReason": null, "restrictReasonType": null, "sourceCitation": null, "startEndPage": null, "stateProvinceRegion": null, "subject": null, "submitterRep": null, "submitterRepAddress": null, "submitterRepCityState": null, "subtype": null, "title": "China Public Hearing Agenda", "topics": null, "trackingNbr": null, "withdrawn": true, "zip": null}, "id": "USTR-2015-0010-0015", "links": {"self": "https://api.regulations.gov/v4/documents/USTR-2015-0010-0015"}, "relationships": {"attachments": {"links": {"related": "https://api.regulations.gov/v4/documents/USTR-2015-0010-0015/attachments", "self": "https://api.regulations.gov/v4/documents/USTR-2015-0010-0015/relationships/attachments"}}}, "type": "documents"}}'
     json_file = json.loads(json_file)
-    expected_path = "data/USTR/USTR-2015-0010/text-USTR-2015-0010/documents"
-    assert expected_path == generator.get_path(json_file)
-
-def test_get_docket_path_full_json(generator):
-    json_file = '{"data": {"attributes": {"additionalRins": null, "address1": null, "address2": null, "agencyId": "USTR", "allowLateComments": false, "authorDate": null, "authors": null, "category": null, "cfrPart": null, "city": null, "comment": null, "commentEndDate": "2015-09-24T03:59:59Z", "commentStartDate": "2015-08-10T04:00:00Z", "country": null, "displayProperties": null, "docAbstract": null, "docketId": "USTR-2015-0010", "documentType": "Notice", "effectiveDate": null, "email": null, "exhibitLocation": null, "exhibitType": null, "fax": null, "field1": null, "field2": null, "fileFormats": [{"fileUrl": "https://downloads.regulations.gov/USTR-2015-0010-0001/content.pdf", "format": "pdf", "size": 182010}, {"fileUrl": "https://downloads.regulations.gov/USTR-2015-0010-0001/content.htm", "format": "htm", "size": 9709}], "firstName": null, "frDocNum": "2015-19523", "frVolNum": null, "govAgency": null, "govAgencyType": null, "implementationDate": null, "lastName": null, "legacyId": null, "media": null, "modifyDate": "2015-10-02T01:31:31Z", "objectId": "0900006481bce624", "ombApproval": null, "openForComment": false, "organization": null, "originalDocumentId": "USTR_FRDOC_0001-0346", "pageCount": 0, "paperLength": 0, "paperWidth": 0, "phone": null, "postedDate": "2015-08-10T04:00:00Z", "postmarkDate": null, "reasonWithdrawn": null, "receiveDate": "2015-08-10T04:00:00Z", "regWriterInstruction": null, "restrictReason": null, "restrictReasonType": null, "sourceCitation": null, "startEndPage": "47985 - 47986", "stateProvinceRegion": null, "subject": null, "submitterRep": null, "submitterRepAddress": null, "submitterRepCityState": null, "subtype": null, "title": "Public Hearing: China\'s Compliance with WTO Commitments", "topics": null, "trackingNbr": null, "withdrawn": false, "zip": null}, "id": "USTR-2015-0010-0001", "links": {"self": "https://api.regulations.gov/v4/documents/USTR-2015-0010-0001"}, "relationships": {"attachments": {"links": {"related": "https://api.regulations.gov/v4/documents/USTR-2015-0010-0001/attachments", "self": "https://api.regulations.gov/v4/documents/USTR-2015-0010-0001/relationships/attachments"}}}, "type": "documents"}}'
-    json_file = json.loads(json_file)
-    expected_path = "data/USTR/USTR-2015-0010/text-USTR-2015-0010/docket"
-    assert expected_path == generator.get_path(json_file)
+    expected_path = "data/USTR/USTR-2015-0010/text-USTR-2015-0010/documents/"
+    assert expected_path == generator.get_document_json_path(json_file)
 
 def test_get_docket_path(generator):
     json = generate_json(id="VETS-2005-0001", type="dockets", agencyId = "VETS")
-    expected_path = "data/VETS/VETS-2005-0001/text-VETS-2005-0001/dockets/VETS-2005-0001.json"
-    assert expected_path == generator.get_path(json)
+    expected_path = "data/VETS/VETS-2005-0001/text-VETS-2005-0001/docket/"
+    assert expected_path == generator.get_docket_json_path(json)
 
 def test_get_docket_path_from_FRDOC_docket(generator):
     json = generate_json(id="VETS_FRDOC_0001", type="dockets", agencyId = "VETS")
-    expected_path = "data/VETS/VETS_FRDOC_0001/text-VETS_FRDOC_0001/dockets/VETS_FRDOC_0001.json"
-    assert expected_path == generator.get_path(json)
+    expected_path = "data/VETS/VETS_FRDOC_0001/text-VETS_FRDOC_0001/docket/"
+    assert expected_path == generator.get_docket_json_path(json)
 
 def test_get_document_path(generator):
     json = generate_json(id = "VETS-2005-0001-0001", type="documents", agencyId="VETS")
-    expected_path = "data/VETS/VETS-2005-0001/text-VETS-2005-0001/documents/VETS-2005-0001-0001.json"
-    assert expected_path == generator.get_path(json)
+    expected_path = "data/VETS/VETS-2005-0001/text-VETS-2005-0001/documents/"
+    assert expected_path == generator.get_document_json_path(json)
 
 def test_get_document_path_from_FRDOC_document(generator):
     json = generate_json(id="VETS_FRDOC_0001-0001", type="documents", agencyId = "VETS")
-    expected_path = "data/VETS/VETS_FRDOC_0001/text-VETS_FRDOC_0001/documents/VETS_FRDOC_0001-0001.json"
-    assert expected_path == generator.get_path(json)
+    expected_path = "data/VETS/VETS_FRDOC_0001/text-VETS_FRDOC_0001/documents/"
+    assert expected_path == generator.get_document_json_path(json)
 
 def test_get_comment_path(generator):
     json = generate_json(id = "USTR-2015-0010-0002", type = "comments", agencyId="USTR")
-    expected_path = "data/USTR/USTR-2015-0010/text-USTR-2015-0010/comments/USTR-2015-0010-0002.json"
-    assert expected_path == generator.get_path(json)
+    expected_path = "data/USTR/USTR-2015-0010/text-USTR-2015-0010/comments/"
+    assert expected_path == generator.get_comment_json_path(json)
 
 def test_get_docket_path_EPA_with_unconvential_agencyId(generator):
     json = generate_json(id = "EPA-HQ-OPP-2011-0939", type="dockets", agencyId="EPA")
-    expected_path = "data/EPA/EPA-HQ-OPP-2011-0939/text-EPA-HQ-OPP-2011-0939/dockets/EPA-HQ-OPP-2011-0939.json"
-    assert expected_path == generator.get_path(json)
+    expected_path = "data/EPA/EPA-HQ-OPP-2011-0939/text-EPA-HQ-OPP-2011-0939/docket/"
+    assert expected_path == generator.get_docket_json_path(json)
 
 def test_get_document_path_EPA_with_unconvential_agencyId(generator):
     json = generate_json(id = "EPA-HQ-OPP-2011-0939-0001", type="documents", agencyId="EPA")
-    expected_path = "data/EPA/EPA-HQ-OPP-2011-0939/text-EPA-HQ-OPP-2011-0939/documents/EPA-HQ-OPP-2011-0939-0001.json"
-    assert expected_path == generator.get_path(json)
+    expected_path = "data/EPA/EPA-HQ-OPP-2011-0939/text-EPA-HQ-OPP-2011-0939/documents/"
+    assert expected_path == generator.get_document_json_path(json)
 
 def test_get_docket_path_with_numbers_in_agencyId(generator):
     json = generate_json(id = "EPA-R08-OAR-2005-UT-0003", type="dockets", agencyId="EPA")
-    expected_path = "data/EPA/EPA-R08-OAR-2005-UT-0003/text-EPA-R08-OAR-2005-UT-0003/dockets/EPA-R08-OAR-2005-UT-0003.json"
-    assert expected_path == generator.get_path(json)
+    expected_path = "data/EPA/EPA-R08-OAR-2005-UT-0003/text-EPA-R08-OAR-2005-UT-0003/docket/"
+    assert expected_path == generator.get_docket_json_path(json)
 
-
-def test_get_comment_path(generator):
-    json = generate_json(id = "USTR-2015-0010-0002", type = "comments", agencyId="USTR")
-    expected_path = "data/USTR/USTR-2015-0010/text-USTR-2015-0010/comments/USTR-2015-0010-0002.json"
-    assert expected_path == generator.get_path(json)
-
-def test_get_docket_path_with_missing_type_key(generator):
-    json = {"data":{
-        "id": "VETS-2005-0001", 
-        "attributes": {
-                    "agencyId":"VETS"
-                }
-        }}
-    with pytest.raises(KeyError):
-        generator.get_path(json)
 
 def test_get_docket_path_with_missing_id_key(generator):
     json = {"data":{
         "type":"dockets",
         "attributes": {
-                    "agencyId":"VETS"
-                }
-        }}
+            "agencyId":"VETS"
+        }
+    }}
     with pytest.raises(KeyError):
-        generator.get_path(json)
+        generator.get_docket_json_path(json)
 
 def test_get_document_path_with_missing_id_key(generator):
     json = {"data":{
@@ -110,7 +89,7 @@ def test_get_document_path_with_missing_id_key(generator):
                 }
         }}
     with pytest.raises(KeyError):
-        generator.get_path(json)
+        generator.get_document_json_path(json)
 
 def test_get_comment_path_with_missing_id_key(generator):
     json = {"data":{
@@ -120,4 +99,4 @@ def test_get_comment_path_with_missing_id_key(generator):
                 }
         }}
     with pytest.raises(KeyError):
-        generator.get_path(json)
+        generator.get_comment_json_path(json)
