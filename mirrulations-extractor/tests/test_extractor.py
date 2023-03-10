f2d5a188a8b60fe023d248a74298accedd8f75cb
Tanishq Iyer <tanishqiyer88@gmail.com>
2023-03-10 14:25:03 -0500

    Add Basic Test for Extractor
    Adds a basic test for the extractor that checks
    that the extractor creates a saved file when given
    an empty pdf.

--- mirrulations-extractor/tests/test_extractor.py
+++ mirrulations-extractor/tests/test_extractor.py
@@ -1,4 +1,27 @@
+import os
+from pytest import fixture
 from mirrextractor.extractor import Extractor
 
-def test_extract_pdf():
-    assert 1 == 2
+
+SAVE_PATH = 'saved/saved.txt'
+
+
+@fixture(autouse=True)
+def remove_saved_file():
+    absolute_path = os.path.dirname(__file__)
+    path = os.path.join(absolute_path, SAVE_PATH)
+
+    if os.path.isfile(path):
+        os.remove(path)
+
+
+def test_extractor_creates_saved_file():
+    absolute_path = os.path.dirname(__file__)
+
+    save_path = os.path.join(absolute_path, SAVE_PATH)
+
+    Extractor.extract_pdf(
+        os.path.join(absolute_path, 'pdfs/empty.pdf'),
+        save_path)
+
+    assert os.path.isfile(save_path)
