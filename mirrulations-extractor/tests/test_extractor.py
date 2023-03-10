48a9d19471d53f5d4012438a552a2a538857a4dc
Tanishq Iyer <tanishqiyer88@gmail.com>
2023-03-10 14:54:45 -0500

    Add Create Saved Dir to Fixture
    saved directory for test files now gets created in
    fixture if it doesn't already exist.

--- mirrulations-extractor/tests/test_extractor.py
+++ mirrulations-extractor/tests/test_extractor.py
@@ -3,16 +3,21 @@ from pytest import fixture
 from mirrextractor.extractor import Extractor
 
 
+SAVE_DIR = 'saved'
 SAVE_PATH = 'saved/saved.txt'
 
 
 @fixture(autouse=True)
 def remove_saved_file():
     absolute_path = os.path.dirname(__file__)
-    path = os.path.join(absolute_path, SAVE_PATH)
+    save_dir = os.path.join(absolute_path, SAVE_DIR)
+    save_path = os.path.join(absolute_path, SAVE_PATH)
 
-    if os.path.isfile(path):
-        os.remove(path)
+    if not os.path.isdir(save_dir):
+        os.mkdir(save_dir)
+
+    if os.path.isfile(save_path):
+        os.remove(save_path)
 
 
 def test_extractor_creates_saved_file():
