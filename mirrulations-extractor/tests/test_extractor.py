228f29d2b32ef1209b1d8bfc3d545f532e467a1e
Tanishq Iyer <tanishqiyer88@gmail.com>
2023-03-13 12:41:25 -0400

    Add Test for Extracting Simple PDF
    Added a test for extracting text from a PDF
    created using google docs that includes the
    text 'a'.

--- mirrulations-extractor/tests/test_extractor.py
+++ mirrulations-extractor/tests/test_extractor.py
@@ -32,6 +32,21 @@ def test_extractor_creates_saved_file():
     assert os.path.isfile(save_path)
 
 
+def test_extractor_extracts_pdf_with_text_a():
+    absolute_path = os.path.dirname(__file__)
+
+    save_path = os.path.join(absolute_path, SAVE_PATH)
+
+    Extractor.extract_text(
+        os.path.join(absolute_path, 'pdfs/a.pdf'),
+        save_path)
+
+    with open(save_path, "r", encoding="utf-8") as in_file:
+        text = in_file.read()
+
+    assert text.strip() == 'a'
+
+
 def test_extractor_doesnt_save_non_pdf():
     absolute_path = os.path.dirname(__file__)
 
