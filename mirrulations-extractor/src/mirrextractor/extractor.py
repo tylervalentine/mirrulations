19a5c08f25e36e53bd5c05f6f64e7f2ec4efd28b
Tyler Valentine <valentinet02@moravian.edu>
2023-03-13 11:11:24 -0400

    Fix extractor

--- mirrulations-extractor/src/mirrextractor/extractor.py
+++ mirrulations-extractor/src/mirrextractor/extractor.py
@@ -1,7 +1,8 @@
+from datetime import datetime
 import os
 import time
 import io
-from datetime import datetime
+import pdfminer
 import pdfminer.high_level
 import pikepdf
 
@@ -13,9 +14,9 @@ class Extractor:
     @staticmethod
     def extract_text(attachment_path, save_path):
         """
-        This method takes a complete path to an attachment
-        and determines which type of extraction will take place.
-
+        This method takes a complete path to an attachment and determines
+        which type of extraction will take place.
+        *Note* save_path is for later use when saving the extracted text
         Parameters
         ----------
         attachment_path : str
@@ -26,13 +27,11 @@ class Extractor:
             ex. /path/to/text/attachment_1.txt
         """
         # gets the type of the attachment file
-        # (ex. /path/to/pdf/attachment_1.pdf -> pdf)
-        file_type = attachment_path[attachment_path.find('.')
-                                    + 1: len(attachment_path)]
-        print(file_type)
+        #   (ex. /path/to/pdf/attachment_1.pdf -> pdf)
+        file_type = attachment_path[attachment_path.rfind('.')+1:]
         if file_type.endswith('pdf'):
             print(f"Extracting text from {attachment_path}")
-            Extractor.extract_pdf(attachment_path, save_path)
+            Extractor._extract_pdf(attachment_path, save_path)
         else:
             print(f"FAILURE: attachment doesn't have appropriate extension \
             {attachment_path}")
@@ -45,12 +44,11 @@ class Extractor:
             .replace('.pdf', '_extracted.txt')
 
     @staticmethod
-    def extract_pdf(attachment_path, save_path):
+    def _extract_pdf(attachment_path, save_path):
         """
         This method takes a complete path to a pdf and stores
         the extracted text in the save_path.
         *Note* If a file exists at save_path, it will be overwritten.
-
         Parameters
         ----------
         attachment_path : str
@@ -61,40 +59,38 @@ class Extractor:
             ex. /path/to/text/attachment_1.txt
         """
         try:
-            pdf = pikepdf.open(attachment_path, allow_overwriting_input=True)
-        except pikepdf.PdfError as error:
-            if isinstance(error.inner_exception, pikepdf.ReadError):
+            pdf = pikepdf.open(attachment_path)
+        except pikepdf.PdfError:
+            try:
                 pdf = pikepdf.open(attachment_path, recover=True)
-            else:
+            except pikepdf.PdfError:
                 print(f"FAILURE: failed to open {attachment_path}")
                 return
-        # Check if the PDF is already linearized
-        # Linearize the PDF
-        pdf.save(attachment_path, linearize=True)
-        # Extract all text from the PDF using pdfminer.six
-        with open(attachment_path, "rb") as save_file:
-            pdf_bytes = io.BytesIO(save_file.read())
+        pdf_bytes = io.BytesIO()
+        pdf.save(pdf_bytes, linearize=True)
         text = pdfminer.high_level.extract_text(pdf_bytes)
         # Save the extracted text to a file
-        with open(save_path, "w", encoding="utf-8") as save_file:
-            save_file.write(text)
+        with open(save_path, "w", encoding="utf-8") as out_file:
+            out_file.write(text)
         print(f"SUCCESS: Saved pdf at {save_path}")
 
 
 if __name__ == '__main__':
     now = datetime.now()
-
     while True:
         for (root, dirs, files) in os.walk('/data'):
             for file in files:
+                # Checks for pdfs
+                if not file.endswith('pdf'):
+                    continue
                 complete_path = os.path.join(root, file)
-                file_save_path = Extractor.make_save_path(complete_path)
-                if not file_save_path.is_file():
+                output_path = "/data/data/" + \
+                    Extractor.make_save_path(complete_path)
+                if not output_path.is_file():
                     start_time = time.time()
-                    Extractor.extract_text(complete_path, file_save_path)
-                    print(f"Time taken to extract text from {complete_path} \
-                    is {start_time - time.time()} seconds")
-
+                    Extractor.extract_text(complete_path, output_path)
+                    print(f"Time taken to extract text from {complete_path}"
+                          f" is {start_time - time.time()} seconds")
         # sleep for a hour
         current_time = now.strftime("%H:%M:%S")
         print(f"Sleeping for an hour : started at {current_time}")
