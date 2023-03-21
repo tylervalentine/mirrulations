1bbda67c0759f0297b78e0834011692d38a6a1ed
Alexander Flores Sosa <alexflore301@gmail.com>
2023-03-21 01:19:30 -0400

    increased coverage for rabbitmq.py by touching exception code lines

--- mirrulations-core/tests/test_rabbitmq.py
+++ mirrulations-core/tests/test_rabbitmq.py
@@ -1,7 +1,8 @@
 from unittest.mock import MagicMock
-
+from mirrcore.job_queue_exceptions import JobQueueException
 from mirrcore.rabbitmq import RabbitMQ
 import pika
+import pytest
 
 """
 This test exists to increase coverage.  The RabbitMQ class encapsulates
@@ -29,6 +30,25 @@ class PikaSpy:
 
     def channel(self, *args, **kwargs):
         return ChannelSpy()
+    
+
+class BadPikaSpy:
+    def __init__(self, *args, **kwargs):
+        self.is_open = True
+
+    def channel(self, *args, **kwargs):
+        return BadConnectionSpy()
+
+
+class BadConnectionSpy:
+    def queue_declare(self, *args, **kwargs):
+        return MagicMock()
+
+    def basic_publish(self, *args, **kwargs):
+        raise pika.exceptions.StreamLostError()
+
+    def basic_get(self, *args, **kwargs):
+        raise pika. exceptions.StreamLostError()
 
 
 def test_rabbit_interactions(monkeypatch):
@@ -39,3 +59,17 @@ def test_rabbit_interactions(monkeypatch):
     r.add('foo')
     r.size()
     r.get()
+
+
+def test_rabbit_error_interactions(monkeypatch):
+    monkeypatch.setattr(pika, 'BlockingConnection', BadPikaSpy)
+
+    rabbitmq = RabbitMQ()
+
+    # Ensure that the exception is raised when add() is called
+    with pytest.raises(JobQueueException):
+        rabbitmq.add('foo')
+
+    # Ensure that the exception is caught and re-raised as a JobQueueException in size()
+    with pytest.raises(JobQueueException):
+        rabbitmq.get()
