d972e93c9bc1e366cff6f2c3074d60355326e387
Evan Toyberg <toyberge@moravian.edu>
2023-03-24 14:41:07 -0400

    Add second flask endpoint for dashboard

--- mirrulations-dashboard/src/mirrdash/dashboard_server.py
+++ mirrulations-dashboard/src/mirrdash/dashboard_server.py
@@ -60,6 +60,12 @@ def create_server(job_queue, docker_server, mongo_client):
         return render_template(
             'index.html'
         )
+    
+    @dashboard.app.route('/dev', methods=['GET'])
+    def _dev():
+        return render_template(
+            'dev.html'
+        )
 
     @dashboard.app.route('/data', methods=['GET'])
     def _get_dashboard_data():
