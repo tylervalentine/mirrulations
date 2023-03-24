ec7cb815afa44d1d59a29fe1592c9e7439064f25
Jack Wagner <jacksonjw519@gmail.com>
2023-03-23 23:27:17 -0400

    Fixes Static Errors

--- mirrulations-work-generator/tests/test_workgen.py
+++ mirrulations-work-generator/tests/test_workgen.py
@@ -62,31 +62,27 @@ def test_work_generator_retries_after_500(requests_mock, mocker):
 
     assert len(requests_mock.request_history) == 2
 
+
 def test_work_generator_output_after_500_error(capsys, requests_mock, mocker):
     mocker.patch('time.sleep')
     results = MockDataSet(150).get_results()
     results.insert(0, {'json': '{}', 'status_code': 504})
     requests_mock.get('https://api.regulations.gov/v4/documents', results)
 
-    database = FakeRedis()
     api = RegulationsAPI('FAKE_KEY')
-    job_queue = JobQueue(database)
+    job_queue = JobQueue(FakeRedis())
     # mock out the rabbit connection
     job_queue.rabbitmq = MockRabbit()
 
-    storage = MockDataStorage()
-    generator = WorkGenerator(job_queue, api, storage)
+    generator = WorkGenerator(job_queue, api, MockDataStorage())
     generator.download('documents')
 
-    captured = capsys.readouterr()
     print_data = [
         'FAILED: https://api.regulations.gov/v4/documents\n',
         '504 Server Error from url ',
         'https://api.regulations.gov/v4/documents?sort=',
         'lastModifiedDatepage[size]=250filter[lastModifiedDate]',
-        '[ge]=1971-12-31+19:00:00page[number]=1\n', 
+        '[ge]=1971-12-31+19:00:00page[number]=1\n',
         'Added any: 150\n'
-
     ]
-    assert captured.out == "".join(print_data)
-
+    assert capsys.readouterr().out == "".join(print_data)
