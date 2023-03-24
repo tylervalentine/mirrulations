a040b143bc155182e454fbd33d753cdd9f8e543b
Tyler Valentine <valentinet02@moravian.edu>
2023-03-24 14:20:18 -0400

    Clean up log statements

--- mirrulations-validation/src/mirrval/job_validator.py
+++ mirrulations-validation/src/mirrval/job_validator.py
@@ -22,19 +22,14 @@ class Validator:
         collection_size = self.datastorage.get_collection_size(endpoint)
         counter = 0
         for result in SearchIterator(self.api, endpoint, beginning_timestamp):
-            print("starting loop")
             print(f"Result was: {result}")
             if result == {}:
-                print("result was empty")
                 continue
             for res in result['data']:
-                print("results were found")
                 if not self.datastorage.exists(res):
-                    print(f"Not in database {res}")
-                    print(res['id'])
-                    print("Adding job to queue")
+                    print(f"Job {res}: {res['id']} not in database, adding to job queue")
                     self.job_queue.add_job(res['links']['self'], res['type'])
-                    print("Finished adding job")
+                    print(f"Finished adding job {res}: {res['id']}")
                 counter += 1
             percentage = (counter / collection_size) * 100
             print(f'{percentage:.2f}%')
@@ -52,34 +47,24 @@ def generate_work(collection=None):
     # api_prefix = collection.upper() if collection else 'DOCKETS'
     api_key = os.getenv("API_KEY")
     api = RegulationsAPI(api_key)
-    print(f"API Key {api_key}")
-    print(f"API {api}")
     storage = DataStorage()
     job_queue = JobQueue(database)
     generator = Validator(api, storage, job_queue)
 
     if not collection:
-        print("not in collection")
         generator.download('dockets')
         generator.download('documents')
         generator.download('comments')
-        print("validator")
     else:
-        print("collection found")
         generator.download(collection)
-        print("done")
 
 
 if __name__ == '__main__':
     job_types = ('dockets', 'documents', 'comments')
     while True:
         if len(sys.argv) > 1 and sys.argv[1] in job_types:
-            print("Doing if statement")
             generate_work(sys.argv[1])
-            print("If statement complete")
         else:
-            print("Doing else statement")
             generate_work()
-            print("Else statement complete")
         print("Validator sleeping for 30 days...")
         time.sleep(60*60*24*30)
