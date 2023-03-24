34d4c83654976471ae61001e7c22f24caa0ce817
Edgar Perez <pereze03@moravian.edu>
2023-03-24 13:37:42 -0400

    added logging throughout job_validator

--- mirrulations-validation/src/mirrval/job_validator.py
+++ mirrulations-validation/src/mirrval/job_validator.py
@@ -22,12 +22,19 @@ class Validator:
         collection_size = self.datastorage.get_collection_size(endpoint)
         counter = 0
         for result in SearchIterator(self.api, endpoint, beginning_timestamp):
+            print("starting loop")
+            print(f"Result was: {result}")
             if result == {}:
+                print("result was empty")
                 continue
             for res in result['data']:
+                print("results were found")
                 if not self.datastorage.exists(res):
+                    print(f"Not in database {res}")
                     print(res['id'])
+                    print("Adding job to queue")
                     self.job_queue.add_job(res['links']['self'], res['type'])
+                    print("Finished adding job")
                 counter += 1
             percentage = (counter / collection_size) * 100
             print(f'{percentage:.2f}%')
@@ -50,18 +57,27 @@ def generate_work(collection=None):
     generator = Validator(api, storage, job_queue)
 
     if not collection:
+        print("not in collection")
         generator.download('dockets')
         generator.download('documents')
         generator.download('comments')
+        print("validator")
     else:
+        print("collection found")
         generator.download(collection)
+        print("done")
 
 
 if __name__ == '__main__':
     job_types = ('dockets', 'documents', 'comments')
     while True:
         if len(sys.argv) > 1 and sys.argv[1] in job_types:
+            print("Doing if statement")
             generate_work(sys.argv[1])
+            print("If statement complete")
         else:
+            print("Doing else statement")
             generate_work()
+            print("Else statement complete")
+        print("Validator sleeping for 30 days...")
         time.sleep(60*60*24*30)
