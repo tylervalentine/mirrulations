2c4abbef835d7aa05a8c0842da60ac4edf5bdfaa
Alexander Flores Sosa <alexflore301@gmail.com>
2023-03-26 20:46:00 -0400

    Added tests for developer dashboard

--- mirrulations-dashboard/tests/test_dashboard.py
+++ mirrulations-dashboard/tests/test_dashboard.py
@@ -67,9 +67,39 @@ def test_dashboard_returns_job_information(mock_server):
     assert results['num_jobs_dockets_queued'] == 1
 
 
+def test_dev_dashboard_returns_container_information(mock_server):
+    client = MagicMock()
+
+    # Mock out the docker object to return Container-like values
+    # for the list method.
+    Container = namedtuple('Container', ['name', 'status'])
+    return_value = [Container('capstone_client1_1', 'running'),
+                    Container('capstone_work_server_1', 'running')]
+    client.containers.list = Mock(return_value=return_value)
+
+    mock_server.docker = client
+    response = mock_server.client.get('/devdata')
+
+    expected = {'client1': 'running',
+                'work_server': 'running'}
+
+    assert response.status_code == 200
+    results = response.get_json()
+    assert results == expected
+
+
 def test_dashboard_returns_html(mock_server):
     add_mock_data_to_database(mock_server.job_queue)
     response = mock_server.client.get('/')
+
+    assert response.status_code == 200
+    expected = '<!DOCTYPE html>'
+    assert response.data.decode().split('\n')[0] == expected
+
+
+def test_developer_dashboard_returns_html(mock_server):
+    add_mock_data_to_database(mock_server.job_queue)
+    response = mock_server.client.get('/dev')
 
     assert response.status_code == 200
     expected = '<!DOCTYPE html>'
