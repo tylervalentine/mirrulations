dc937ef840642980a0aa03c370442e18fed96c9e
Justin Szaro <justinszaro@gmail.com>
2023-03-28 11:56:37 -0400

    Remove comments

--- mirrulations-core/src/mirrcore/data_storage.py
+++ mirrulations-core/src/mirrcore/data_storage.py
@@ -27,13 +27,6 @@ class DataStorage:
                 self.comments.insert_one(data)
 
     def add_attachment(self, data):
-        # if 'attachments_text' in data.keys():
-        #     for attachment_text in data['data']['attachments_text']:
-        #             data = {'id':data['data']['id'], 'text':attachment_text}
-        #             self.attachments.insert_one(data)
-        # Updated this line to no longer be a for loop
-        # This was causing the large attachments downloaded counter since we
-        # were making an entry for each key in data['results'] before
         entry = {'path': data['attachment_path'],
                  'file': data['attachment_filename']}
         self.attachments.insert_one(entry)
