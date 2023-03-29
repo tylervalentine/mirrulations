5549a40c0aa6ac1f5ea5058d63cb8da5c6903149
Jack Wagner <jacksonjw519@gmail.com>
2023-03-29 00:55:18 -0400

    Add S3 saving function as well as boto3 requirements

--- mirrulations-client/src/mirrclient/saver.py
+++ mirrulations-client/src/mirrclient/saver.py
@@ -1,6 +1,8 @@
 import os
+import json
 from json import dumps, load
-
+import boto3
+import botocore
 
 class Saver:
     """
@@ -68,3 +70,24 @@ class Saver:
     def check_for_duplicates(self, path, data, i):
         if self.is_duplicate(self.open_json_file(path), data) is False:
             self.save_duplicate_json(path, data, i)
+
+    def establish_s3_connection(self):
+        """
+        Checks if a valid connection could be made to s3
+        """
+        try:
+            session = boto3.Session(profile_name="profile_name")
+            s3_client = session.client("s3")
+            return s3_client
+        except Exception:
+            return False
+        
+
+    def save_to_s3(self, path, data):
+        s3_client = self.establish_s3_connection()
+        try:
+            s3_client.put_object(Bucket="test-mirrulations", Key=f'{path}', Body=json.dumps(data))
+            print("SUCCESS: Wrote results to S3")
+
+        except botocore.exceptions.ClientError:
+            print("FAILURE: Writing to S3")
