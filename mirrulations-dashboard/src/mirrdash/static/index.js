37745470b81ab12abd115c4511379a9e28aace25
Edgar Perez <78161259+pereze03@users.noreply.github.com>
2023-03-29 14:54:01 -0400

    Removing client 26

--- mirrulations-dashboard/src/mirrdash/static/index.js
+++ mirrulations-dashboard/src/mirrdash/static/index.js
@@ -114,7 +114,6 @@ const updateDeveloperDashboardData = () => {
             client23,
             client24,
             client25,
-            client26,
             nginx,
             mongo,
             redis,
@@ -148,7 +147,6 @@ const updateDeveloperDashboardData = () => {
         updateStatus('client23-status', client23)
         updateStatus('client24-status', client24)
         updateStatus('client25-status', client25)
-        updateStatus('client26-status', client26)
         updateStatus('nginx-status', nginx)
         updateStatus('mongo-status', mongo)
         updateStatus('redis-status', redis);
