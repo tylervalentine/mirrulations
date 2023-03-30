0905210e1bc1fa7e332fa5f89a1886e38c6c8b65
Justin Szaro <justinszaro@gmail.com>
2023-03-30 15:08:52 -0400

    Test put_binary function

--- mirrulations-core/tests/test_amazon_s3.py
+++ mirrulations-core/tests/test_amazon_s3.py
@@ -30,3 +30,14 @@ def test_put_text_to_bucket():
     s3_bucket.put_text_s3(test_path, test_data)
     body = conn.Object("test-mirrulations1", "data/test").get()["Body"].read().decode("utf-8")
     assert body == '{"data": "test"}'
+
+@mock_s3
+def test_put_binary_to_bucket():
+    conn = create_mock_mirrulations_bucket()
+    s3_bucket = AmazonS3(bucket_name ="test-mirrulations1")
+    test_data = b'\x17'
+    test_path = "data/test"
+    s3_bucket.put_binary_s3(test_path, test_data)
+    body = conn.Object("test-mirrulations1", "data/test").get()["Body"].read().decode("utf-8")
+    assert body == '\x17'
+
