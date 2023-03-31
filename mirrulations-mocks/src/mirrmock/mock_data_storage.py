6c44ee2e1f8c9173f9520cb33023ac6fc5783adb
Bryan Cohen <82907967+bryantqr@users.noreply.github.com>
2023-03-31 12:28:27 -0400

    added get_collection_size function that is used on the job validator for test purposes

--- mirrulations-mocks/src/mirrmock/mock_data_storage.py
+++ mirrulations-mocks/src/mirrmock/mock_data_storage.py
@@ -4,6 +4,7 @@ class MockDataStorage:
     def __init__(self):
         self.added = []
         self.attachments_added = []
+        self.collection_size = 1
 
     # pylint: disable=unused-argument, no-self-use
     def exists(self, search_element):
@@ -14,3 +15,7 @@ class MockDataStorage:
 
     def add_attachment(self, data):
         self.attachments_added.append(data)
+
+    # mock the get_collection_size function
+    def get_collection_size(self, data):
+        return self.collection_size
