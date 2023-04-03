703b342e401b114af7673025b5bb676af7c365aa
Tanishq Iyer <tanishqiyer88@gmail.com>
2023-04-03 18:35:56 -0400

    Add load_redis function
    Code to load redis is duplicated in several
    packages.
    I added a load_redis function to avoid this
    duplication.

--- mirrulations-core/tests/test_is_redis_available.py
+++ mirrulations-core/tests/test_is_redis_available.py
@@ -1,6 +1,7 @@
 
-from mirrcore.redis_check import is_redis_available
+from mirrcore.redis_check import is_redis_available, load_redis
 from mirrmock.mock_redis import BusyRedis, ReadyRedis
+from unittest.mock import patch
 
 
 def test_when_redis_loading_is_unavailable():
@@ -13,3 +14,15 @@ def test_when_redis_done_loading_is_available():
     database = ReadyRedis()
     is_available = is_redis_available(database)
     assert is_available is True
+
+
+@patch('mirrcore.redis_check.is_redis_available', return_value=False)
+@patch('time.sleep', return_value=None)
+def test_sleeps_when_redis_is_unavailable(patched_available, patched_sleep):
+    def side_effect(x):
+        patched_available.return_value = True
+    patched_sleep.side_effect = side_effect
+
+    load_redis()
+
+    patched_sleep.assert_called_once()
