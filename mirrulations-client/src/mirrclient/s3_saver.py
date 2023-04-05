7cd128e9c1e8e160f5713c6a9b74b197d29e6e97
Jack Wagner <jacksonjw519@gmail.com>
2023-04-05 00:29:15 -0400

    S3Saver Class documentation

--- mirrulations-client/src/mirrclient/s3_saver.py
+++ mirrulations-client/src/mirrclient/s3_saver.py
@@ -5,8 +5,32 @@ import boto3
 
 
 class S3Saver():
+    """
+    A class which handles saving to an S3 bucket
+    ...
+    Methods
+    -------
+    get_s3_client()
 
+    get_credentials() -> boto3.Client
+
+    save_json(path = string, data = dict)
+
+    save_binary(path = string, binary = bytes)
+
+    """
     def __init__(self, bucket_name="mirrulations"):
+        """
+        Constructor for S3Saver
+        Gets AWS credentials from .env file
+        Establishes S3 client connection
+        Sets the bucket name (default='mirrulations')
+
+        Parameters
+        -------
+        bucket_name : str
+            Name of the bucket to write data to.
+        """
         self.get_credentials()
         self.access_key = None
         self.secret_access_key = None
@@ -14,6 +38,9 @@ class S3Saver():
         self.bucket_name = bucket_name
 
     def get_s3_client(self):
+        """
+        Returns S3 client connection using aws credentials
+        """
         if self.get_credentials() is False:
             print("No credentials provided, Unable to connect to S3")
         return boto3.client(
@@ -24,6 +51,11 @@ class S3Saver():
                 )
 
     def get_credentials(self):
+        """
+        Loads aws credentials from .env file
+        Saves credentials to instance variables
+
+        """
         load_dotenv()
         self.access_key = os.getenv("AWS_ACCESS_KEY")
         self.secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
@@ -31,6 +63,17 @@ class S3Saver():
                 self.secret_access_key is not None)
 
     def save_json(self, path, data):
+        """
+        Saves json file to Amazon S3 bucket
+
+        Parameters
+        -------
+        path : str
+            Where to save the data to in the S3 bucket
+
+        data : dict
+            The json as a dict to save.
+        """
         response = self.s3_client.put_object(
             Bucket=self.bucket_name,
             # Path needs to be data/xxx
@@ -40,11 +83,22 @@ class S3Saver():
         print(f"SUCCESS: Wrote json to S3: {path}")
         return response
 
-    def save_binary(self, path, data):
+    def save_binary(self, path, binary):
+        """
+        Saves json file to Amazon S3 bucket
+
+        Parameters
+        -------
+        path : str
+            Where to save the data to in the S3 bucket
+
+        binary : bytes
+            The binary response.content returns
+        """
         response = self.s3_client.put_object(
             Bucket=self.bucket_name,
             # Path needs to be data/xxx
             Key=path,
-            Body=data)
+            Body=binary)
         print(f"SUCCESS: Wrote binary to S3: {path}")
         return response
