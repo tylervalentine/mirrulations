3f9124b7059cb99e6c7822b3affe48b9c1425df1
Jack Wagner <jacksonjw519@gmail.com>
2023-04-05 00:17:30 -0400

    Saver documentation

--- mirrulations-client/src/mirrclient/saver.py
+++ mirrulations-client/src/mirrclient/saver.py
@@ -1,23 +1,51 @@
 class Saver:
     """
-    A class which takes the content of a json or binary file
-    (pdf, doc, etc.), and saves it using a certain method.
+    A class which encapsulates the saving for the Client
+    A Saver has a list of savers which are other classes
     ...
     Methods
     -------
     save_json(path = string, data = response)
-        Calls each Saver Objects save_json method
-        and saves the file in the appropriate way
 
     save_binary(path = string, data, = response.content)
     """
     def __init__(self, savers=None) -> None:
+        """
+        Parameters
+        ----------
+        savers : list
+            A list of Saver Objects Ex: S3Saver(), DiskSaver()
+        """
         self.savers = savers
 
     def save_json(self, path, data):
+        """
+        Iterates over the instance variable savers list
+        and calls the corresponding subclass save_json() method.
+
+        Parameters
+        ----------
+        path : str
+            A string denoting where the json file should be saved to.
+
+        data: dict
+            The json as a dict to save.
+        """
         for saver in self.savers:
             saver.save_json(path, data)
 
-    def save_binary(self, path, data):
+    def save_binary(self, path, binary):
+        """
+        Iterates over the instance variable savers list
+        and calls the corresponding subclass save_binary() method.
+
+        Parameters
+        ----------
+        path : str
+            A string denoting where the binary file should be saved to.
+
+        binary: bytes
+            The binary response.content returns.
+        """
         for saver in self.savers:
-            saver.save_binary(path, data)
+            saver.save_binary(path, binary)
