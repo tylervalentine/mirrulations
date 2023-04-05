6a8970f5bb61f53e64ecca64431e51ac25de5df3
Bryan Cohen <82907967+bryantqr@users.noreply.github.com>
2023-04-05 14:09:55 -0400

    fixed lint errors

--- mirrulations-core/tests/test_data_counts.py
+++ mirrulations-core/tests/test_data_counts.py
@@ -2,9 +2,10 @@ import unittest
 from unittest.mock import MagicMock, patch
 from mirrcore.data_counts import DataCounts
 
+
 class TestDataCounts(unittest.TestCase):
 
-# Test that each count function returns the expected values
+    # Test that each count function returns the expected values
     @patch('requests.get')
     def test_get_counts(self, mock_api_request):
         """Tests that each count function returns the expected value"""
@@ -18,27 +19,21 @@ class TestDataCounts(unittest.TestCase):
         comments_response.json.return_value = {"meta": {"totalElements": 2500}}
         mock_api_request.side_effect = [dockets_response, documents_response, comments_response]
 
-
         # Call the function we want to test
         counts = DataCounts(api_key).get_counts()
-
 
         # Make assertions about the function's behavior
         self.assertEqual(counts, [500, 1000, 2500])  # Expected results
 
-     # Test that __get_total_elements extracts the correct value from a response object
+    # Test that __get_total_elements extracts the correct value from a response object
     def test__get_total_elements(self):
-       """Tests that __get_total_elements correctly extracts the 'totalElements' value from a response"""
-       # Set up mock data and objects
-       response = MagicMock()
-       response.json.return_value = {"meta": {"totalElements": 123}}
+        """Tests that __get_total_elements correctly extracts the 'totalElements' value from a response"""
+        # Set up mock data and objects
+        response = MagicMock()
+        response.json.return_value = {"meta": {"totalElements": 123}}
 
+        # Call the function we want to test
+        total_elements = DataCounts(api_key="test")._DataCounts__get_total_elements(response)
 
-       # Call the function we want to test
-       total_elements = DataCounts(api_key="test")._DataCounts__get_total_elements(response)
-
-
-       # Make assertions about the function's behavior
-       self.assertEqual(total_elements, 123)  # Expected result
-
-
+        # Make assertions about the function's behavior
+        self.assertEqual(total_elements, 123)  # Expected result
