9a35afb61058f391b8263735a50f6ec6920dfe6f
Jack Wagner <jacksonjw519@gmail.com>
2023-04-04 23:36:11 -0400

    Saver Refactor into parent class

--- mirrulations-client/src/mirrclient/saver.py
+++ mirrulations-client/src/mirrclient/saver.py
@@ -1,70 +1,23 @@
-import os
-from json import dumps, load
-
-
 class Saver:
     """
-    A class which takes the content of an attachment (pdf, doc, etc.), and
-    saves it to a given path.
+    A class which takes the content of a json or binary file
+    (pdf, doc, etc.), and saves it using a certain method.
     ...
     Methods
     -------
-    save(content = response, path = string)
-        Takes the content (pdf, doc, etc.) and saves the attachment to a
-        given path
+    save_json(path = string, data = response)
+        Calls each Saver Objects save_json method
+        and saves the file in the appropriate way
+    
+    save_binary(path = string, data, = response.content)
     """
-
-    def make_path(self, path):
-        try:
-            os.makedirs(f'/data{path}')
-        except FileExistsError:
-            print(f'Directory already exists in root: /data{path}')
-
-    def save_to_disk(self, path, data):
-        with open(path, 'x', encoding='utf8') as file:
-            print('Writing results to disk')
-            file.write(dumps(data))
+    def __init__(self, savers=None) -> None:
+        self.savers = savers
 
     def save_json(self, path, data):
-        """
-        writes the results to disk. used by docket document and comment jobs
+        for saver in self.savers:
+            saver.save_json(path, data)
 
-        Parameters
-        ----------
-        data : dict
-            the results data to be written to disk
-        """
-        data = data['results']
-        if os.path.exists(path) is False:
-            self.save_to_disk(path, data)
-        else:
-            self.check_for_duplicates(path, data, 1)
-
-    def save_duplicate_json(self, path, data, i):
-        path_without_file_type = path.strip(".json")
-        path = f'{path_without_file_type}({i}).json'
-        if os.path.exists(path) is False:
-            print(f'JSON is different than duplicate: Labeling ({i})')
-            self.save_to_disk(path, data)
-        else:
-            self.check_for_duplicates(path, data, i + 1)
-
-    def save_attachment(self, path, data):
-        with open(path, "wb") as file:
-            file.write(data)
-            file.close()
-
-    def open_json_file(self, path):
-        with open(path, encoding='utf8') as file:
-            saved_data = load(file)
-        return saved_data
-
-    def is_duplicate(self, existing, new):
-        if existing == new:
-            print('Data is a duplicate, skipping this download')
-            return True
-        return False
-
-    def check_for_duplicates(self, path, data, i):
-        if self.is_duplicate(self.open_json_file(path), data) is False:
-            self.save_duplicate_json(path, data, i)
+    def save_binary(self, path, data):
+        for saver in self.savers:
+            saver.save_binary(path, data)
