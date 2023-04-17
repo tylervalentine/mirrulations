a080a5c200e6b426ff35f11b5a2778c6fcdf1038
Kyle Smilon <smilonk@moravian.edu>
2023-04-17 16:56:51 -0400

    Added totals for documents, comments, attachments to dashboard

--- mirrulations-dashboard/src/mirrdash/static/index.js
+++ mirrulations-dashboard/src/mirrdash/static/index.js
@@ -115,12 +115,15 @@ const updateClientDashboardData = () => {
         updateJobTypeProgress("attachments-done",num_attachments_done, regulations_total_attachments); 
         // Counts for numbers
         updateCount("dockets-done",num_dockets_done);
-        updateCount("regulations-total-dockets", regulations_total_dockets)
         updateCount("documents-done",num_documents_done);
         updateCount("comments-done",num_comments_done);
         updateCount("attachments-done",num_attachments_done);
         updateCount("pdf-attachments-done", num_pdf_attachments_done)
         updateCount("pdf-extractions-done", num_extractions_done) 
+        updateCount("regulations-total-dockets", regulations_total_dockets)
+        updateCount("regulations-total-documents", regulations_total_documents)
+        updateCount("regulations-total-comments", regulations_total_comments)
+        updateCount("regulations-total-attachments", regulations_total_attachments)
         updateJobsQueuedByType("comments-queued", num_jobs_comments_queued);
         updateJobsQueuedByType("dockets-queued", num_jobs_dockets_queued);
         updateJobsQueuedByType("documents-queued", num_jobs_documents_queued);
