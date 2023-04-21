0bf615116f1d1e47e040c9b5ccf63304e87d51f4
Edwin <cojitamboe@moravian.edu>
2023-04-21 15:51:07 -0400

    Made values display unknown if Redis is down

--- mirrulations-dashboard/src/mirrdash/static/index.js
+++ mirrulations-dashboard/src/mirrdash/static/index.js
@@ -3,6 +3,7 @@
 const BASE_URL = window.location.href;
 const RADIUS = 80;
 const NUMBER_ANIMATION_STEP = 4;
+let unknown = false;
 
 window.addEventListener('load', function init() {
     if (window.location.pathname === '/') {
@@ -18,8 +19,8 @@ const updateHtmlValues = (jobsWaiting, jobsDone) => {
     if (jobsWaiting === null || jobsDone === null) {
         // Handle the case where value or total is null,
         // indicating Job Queue Error from dashboard
-        console.log("got into the if")
-        document.getElementById('jobs-waiting-number').textContent = "Error"; // todo here
+        unknown = true;
+        document.getElementById('jobs-waiting-number').textContent = "Unknown";
     }
     else {
         let ids = ['jobs-waiting', 'jobs-done']
@@ -46,13 +47,16 @@ const updateCorpusProgressHtml = (jobTypeCountsDone, totalCorpus) => {
         currentProgress += jobTypeCountsDone[i]
     }
     let percent = (currentProgress/totalCorpus) * 100;
-    document.getElementById('progress-to-corpus-bar-percentage').textContent = `${percent.toFixed(2)}%`
+    if (!unknown) {
+        document.getElementById('progress-to-corpus-bar-percentage').textContent = `${percent.toFixed(2)}%`
+    } else {
+        document.getElementById('progress-to-corpus-bar-percentage').textContent = `Unknown`
+    }
     const progressBar = document.querySelector('.progress-bar-to-corpus');
 
     // Set the width of the progress bar to the calculated percentage
     progressBar.style.width = `${percent}%`;
 }
-
 
 const updateStatus = (container, status) => {
         let status_span = document.getElementById(container)
@@ -67,13 +71,25 @@ const updateStatus = (container, status) => {
 
 }
 
-const updateCount = (id, value) => {
-    document.getElementById(id+'-number').textContent = value.toLocaleString('en');
+const updateJobTypeProgress = (id, value, total) => {
+    if (!unknown) {
+        let percent = (value/total) * 100;
+        document.getElementById(id+'-percent').textContent = `${percent.toFixed(2)}%`;
+    } else {
+        document.getElementById(id+'-percent').textContent = "Unknown";
+    }
 }
 
-const updateJobTypeProgress = (id, value, total) => {
-    let percent = (value/total) * 100;
-    document.getElementById(id+'-percent').textContent = `${percent.toFixed(2)}%`;
+const updateCount = (id, value, is_pdf) => {
+    if (!unknown) {
+        document.getElementById(id+'-number').textContent = value.toLocaleString('en');
+    } else {
+        if (is_pdf) {
+            document.getElementById(id+'-number').textContent = "Unknown"
+        } else {
+            document.getElementById(id+'-number').textContent = " "
+        }
+    }
 }
 
 const updateJobsQueuedByType = (id, value) => {
@@ -119,7 +135,7 @@ const updateClientDashboardData = () => {
         updateCount("documents-done",num_documents_done);
         updateCount("comments-done",num_comments_done);
         updateCount("attachments-done",num_attachments_done);
-        updateCount("pdf-attachments-done", num_pdf_attachments_done)
+        updateCount("pdf-attachments-done", num_pdf_attachments_done, true)
         updateCount("pdf-extractions-done", num_extractions_done) 
         updateJobsQueuedByType("comments-queued", num_jobs_comments_queued);
         updateJobsQueuedByType("dockets-queued", num_jobs_dockets_queued);
