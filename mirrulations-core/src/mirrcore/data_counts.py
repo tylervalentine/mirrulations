055bd5f86e716b3a19730915aa629c2b0f29b056
Evan Toyberg <toyberge@moravian.edu>
2023-04-23 19:38:32 -0400

    Implement DataNotFoundException in DataCounts
    Created a generalized exception for handling errors leaving the DataCounts class. This encapsulates all of the requests exceptions, and invalid data formatting possibilities (KeyError, not
    an int, less than 0).
    /

--- mirrulations-core/src/mirrcore/data_counts.py
+++ mirrulations-core/src/mirrcore/data_counts.py
@@ -1,3 +1,4 @@
+import requests
 from mirrcore.regulations_api import RegulationsAPI
 
 
@@ -23,6 +24,11 @@ class DataCounts:
         dockets = self._get_dockets_count()
         documents = self._get_documents_count()
         comments = self._get_comments_count()
+
+        for data in [dockets, documents, comments]:
+            if not isinstance(data, int) or data < 0:
+                raise DataNotFoundException("Invalid data")
+
         return [dockets, documents, comments]
 
     def _get_dockets_count(self):
@@ -30,7 +36,7 @@ class DataCounts:
         Get the number of docket entries on Regulations.gov
         @return integer count of docket entries
         """
-        response = self.regulations_api.download(f'{self.url}/{"dockets"}')
+        response = self.__make_api_call("dockets")
         return self.__get_total_elements(response)
 
     def _get_documents_count(self):
@@ -38,7 +44,7 @@ class DataCounts:
         Get the number of document entries on Regulations.gov
         @return integer count of document entries
         """
-        response = self.regulations_api.download(f'{self.url}/{"documents"}')
+        response = self.__make_api_call("documents")
         return self.__get_total_elements(response)
 
     def _get_comments_count(self):
@@ -46,11 +52,35 @@ class DataCounts:
         Get the number of comment entries on Regulations.gov
         @return integer count of comment entries
         """
-        response = self.regulations_api.download(f'{self.url}/{"comments"}')
+        response = self.__make_api_call("comments")
         return self.__get_total_elements(response)
 
     def __get_total_elements(self, response):
         """
         Get the total number of elements from the response
+        @param response: json response from Regulations.gov
+        @return integer count of elements
         """
-        return response['meta']['totalElements']
+        try:
+            return response['meta']['totalElements']
+        except KeyError as exc:
+            raise DataNotFoundException("Improper JSON structure") from exc
+
+    def __make_api_call(self, endpoint):
+        """
+        Make an API call to Regulations.gov
+        @param endpoint: string "dockets", "documents", or "comments"
+        @return json response from Regulations.gov
+        """
+        try:
+            response = self.regulations_api.download(f'{self.url}/{endpoint}')
+        except requests.exceptions.RequestException as exc:
+            raise DataNotFoundException from exc
+        return response
+
+
+class DataNotFoundException(Exception):
+    """
+    Generalized exception raised when data is not what
+    is expected from Regulations.gov
+    """
